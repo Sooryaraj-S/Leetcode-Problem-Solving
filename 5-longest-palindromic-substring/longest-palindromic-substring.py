bf8d7691057526750747c48eb37a8e360f8c66e2
Sooryaraj S <sooryaraj.dev@gmail.com>
2026-02-24 20:16:12 +0530

    Time: 2527 ms (21.12%) | Memory: 20.2 MB (8.34%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        n = len(s)
+        if n <= 1:
+            return s
+        
+        # Initialize a 2D table for dynamic programming
+        dp = [[False] * n for _ in range(n)]
+        start, max_len = 0, 1
+        
+        # All single characters are palindromes
+        for i in range(n):
+            dp[i][i] = True
+        
+        # Check for substrings of length 2
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                start = i
+                max_len = 2
+        
+        # Check for substrings of length > 2
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    if length > max_len:
+                        start = i
+                        max_len = length
+        
+        return s[start:start + max_len]
+        
